8c2f9294b47c1f9ce40f4cc728046474e219c9ca
Bergyo24 <andreabergesio32@gmail.com>
2022-06-21 15:13:01 +0200

    Made the font size bigger in the stats section

--- src/components/PokeSpec.js
+++ src/components/PokeSpec.js
@@ -8,15 +8,15 @@ export const PokeSpec = ({poke}) => {
       </figure>
       <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
         <h3 style={{width:'100%', textAlign:'center'}}>Name:{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</h3>
-        <h4 style={{width:'50%',textAlign:'center',borderTop:'1px solid grey', paddingTop:'5%'}}>Stats:</h4>
-        <p>Weight: {(poke.weight)/10}kg </p>
-        <p>Height: {(poke.height)/10}m </p>
-        <p>Base Exp: {poke.base_experience}</p>
+        <h3 style={{width:'50%',textAlign:'center',borderTop:'1px solid grey', paddingTop:'5%'}}>Stats:</h3>
+        <p style={{fontSize:'1.5rem'}}>Weight: {(poke.weight)/10}kg </p>
+        <p style={{fontSize:'1.5rem'}}>Height: {(poke.height)/10}m </p>
+        <p style={{fontSize:'1.5rem'}}>Base Exp: {poke.base_experience}</p>
         <div style={{display:'flex', alignItems:'center', justifyContent:'space-around', width: '17%'}}>
-          <p>Type:</p>
+          <p style={{fontSize:'1.5rem'}}>Type:</p>
           <ul style={{display:'inline-block',listStyle:'none',paddingLeft:'0', listStylePosition:'inside' }}>            
             {poke.types.map(({type}, index) =>{
-                return <li key={index}>{type.name.charAt(0).toUpperCase() + type.name.slice(1)}</li>
+                return <li key={index}  style={{fontSize:'1.5rem'}}>{type.name.charAt(0).toUpperCase() + type.name.slice(1)}</li>
           })}
           </ul>
         </div>
